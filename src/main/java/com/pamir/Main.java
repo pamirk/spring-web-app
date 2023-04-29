f49157f74b2177c06fa494b6791d5aa346ec5f9a
Pamir khan <pmysteryk@gmail.com>
2023-04-29 23:58:36 +0500

    Feat: Added GreetRequest record to return json response.

--- src/main/java/com/pamir/Main.java
+++ src/main/java/com/pamir/Main.java
@@ -3,6 +3,7 @@ package com.pamir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -18,5 +19,11 @@ public class Main {
 		return new GreetResponse("Hello");
 	}
 
+	// generated http post request
+
+
+
 	record GreetResponse(String greet) {}
+
+	record GreetRequest(String name) {}
 }
