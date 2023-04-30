21e3e19db5ecea2ff0bc5257cdc4b0dd12e4e28b
Pamir khan <pmysteryk@gmail.com>
2023-05-01 01:27:55 +0500

    feat: added CustomerService class

---
+++ src/main/java/com/pamir/customer/CustomerService.java
@@ -0,0 +1,21 @@
+package com.pamir.customer;
+
+import java.util.List;
+
+public class CustomerService {
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerDao.selectAllCustomers();
+    }
+
+    public Customer getCustomerById(Integer id) {
+        return customerDao.selectCustomerById(id)
+                .orElseThrow(() -> new IllegalStateException("Customer with id [%s] does not exists".formatted(id)));
+
+    }
+}
