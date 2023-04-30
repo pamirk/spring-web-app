a345e188465d97c5e5182ddba2310332cc3c613e
Pamir khan <pmysteryk@gmail.com>
2023-05-01 01:00:01 +0500

    fix: extracted Customer class in seperate file.

--- src/main/java/com/pamir/Main.java
+++ src/main/java/com/pamir/Main.java
@@ -1,5 +1,6 @@
 package com.pamir;
 
+import com.pamir.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,78 +38,4 @@ public class Main {
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("Customer with id [%s] does not exists".formatted(id)));
     }
-
-    public static class Customer {
-        Integer id;
-        String name;
-        String email;
-        Integer age;
-
-        public Customer() {
-        }
-
-        public Customer(Integer id, String name, String email, Integer age) {
-            this.id = id;
-            this.name = name;
-            this.email = email;
-            this.age = age;
-        }
-
-        public Integer getId() {
-            return id;
-        }
-
-        public void setId(Integer id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-
-        public Integer getAge() {
-            return age;
-        }
-
-        public void setAge(Integer age) {
-            this.age = age;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Customer customer = (Customer) o;
-            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name, email, age);
-        }
-
-        @Override
-        public String
-        toString() {
-            return "Customer{" +
-                    "id='" + id + '\'' +
-                    ", name='" + name + '\'' +
-                    ", email='" + email + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
-    }
-
 }
