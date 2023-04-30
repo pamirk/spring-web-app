87496fcf2fad6ec3ec26a7aa7c6ef6588e4e48dd
Pamir khan <pmysteryk@gmail.com>
2023-05-01 01:26:03 +0500

    feat: add Customer Dao class

--- src/main/java/com/pamir/Main.java
+++ src/main/java/com/pamir/Main.java
@@ -1,41 +1,13 @@
 package com.pamir;
 
-import com.pamir.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
-@RestController
 public class Main {
-
-    private static List<Customer> customers;
-
-    static {
-        customers = List.of(
-                new Customer(1, "John", "john@gmail.com", 20),
-                new Customer(2, "Jamila", "Jamila@gmail.com", 30));
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("api/v1/customers")
-    public List<Customer> getCustomers() {
-        return customers;
-    }
-
-    @GetMapping("api/v1/customers/{id}")
-    public Customer getCustomer(@PathVariable("id") Integer id) {
-        return customers.stream()
-                .filter(customer -> customer.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("Customer with id [%s] does not exists".formatted(id)));
-    }
 }
---
+++ src/main/java/com/pamir/customer/CustomerDao.java
@@ -0,0 +1,12 @@
+package com.pamir.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+
+    List<Customer> selectAllCustomers();
+
+    Optional<Customer> selectCustomerById(Integer id);
+
+}
