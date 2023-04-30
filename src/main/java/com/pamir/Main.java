8b00aa4baf59f63992374246f67b4c585e148caa
Pamir khan <pmysteryk@gmail.com>
2023-04-30 20:40:07 +0500

    fix: added id attribute in Customer Model

--- src/main/java/com/pamir/Main.java
+++ src/main/java/com/pamir/Main.java
@@ -3,8 +3,6 @@ package com.pamir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +15,9 @@ public class Main {
     private static List<Customer> customers;
 
     static {
-        customers = List.of(new Customer("John", "john@gmail.com", 20), new Customer("Jamila", "Jamila@gmail.com", 30));
+        customers = List.of(
+                new Customer(1, "John", "john@gmail.com", 20),
+                new Customer(2, "Jamila", "Jamila@gmail.com", 30));
     }
 
     public static void main(String[] args) {
@@ -30,6 +30,7 @@ public class Main {
     }
 
     public static class Customer {
+        Integer id;
         String name;
         String email;
         Integer age;
@@ -37,10 +38,19 @@ public class Main {
         public Customer() {
         }
 
-        public Customer(String name, String email, Integer age) {
+        public Customer(Integer id, String name, String email, Integer age) {
+            this.id = id;
             this.name = name;
             this.email = email;
             this.age = age;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
         }
 
         public String getName() {
@@ -72,17 +82,23 @@ public class Main {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Customer customer = (Customer) o;
-            return Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(name, email, age);
+            return Objects.hash(id, name, email, age);
         }
 
         @Override
-        public String toString() {
-            return "Customer{" + "name='" + name + '\'' + ", email='" + email + '\'' + ", age=" + age + '}';
+        public String
+        toString() {
+            return "Customer{" +
+                    "id='" + id + '\'' +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
         }
     }
 
