ec51bf9083a94cb1d8d1bee8e0926cf8c19258ca
Pamir khan <pmysteryk@gmail.com>
2023-05-01 19:10:03 +0500

    feat: added response status annotation to return 404 status code

--- src/main/java/com/pamir/exception/ResourceNotFound.java
+++ src/main/java/com/pamir/exception/ResourceNotFound.java
@@ -1,5 +1,10 @@
 package com.pamir.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found")
 public class ResourceNotFound extends RuntimeException {
 
     public ResourceNotFound(String message) {
