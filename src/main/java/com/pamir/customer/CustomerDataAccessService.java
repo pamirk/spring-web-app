1dd248ecefe1d3b86cd1d38a7e96b9404ce20303
Pamir khan <pmysteryk@gmail.com>
2023-05-01 12:00:53 +0500

    feat: added required annotations.

--- src/main/java/com/pamir/customer/CustomerDataAccessService.java
+++ src/main/java/com/pamir/customer/CustomerDataAccessService.java
@@ -1,12 +1,15 @@
 package com.pamir.customer;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class CustomerDataAccessService implements CustomerDao {
 
-
-    private static List<Customer> customers;
+    //fake database
+    private static final List<Customer> customers;
 
     static {
         customers = List.of(new Customer(1, "John", "john@gmail.com", 20), new Customer(2, "Jamila", "Jamila@gmail.com", 30));
