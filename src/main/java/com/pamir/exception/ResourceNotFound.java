4405fef08b3ef2cf6f1e5339e64b45779852f791
Pamir khan <pmysteryk@gmail.com>
2023-05-01 19:19:07 +0500

    feat: added include-message=always to show error message when error occurs

--- src/main/java/com/pamir/exception/ResourceNotFound.java
+++ src/main/java/com/pamir/exception/ResourceNotFound.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Resource not found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFound extends RuntimeException {
 
     public ResourceNotFound(String message) {
