97a6818bc8cbf937f52061d4ee3cc0a7228c2161
Pamir khan <pmysteryk@gmail.com>
2023-05-02 13:50:24 +0500

    fix: renamed CustomerDataAccessService to CustomerListDataAccessService

--- src/main/java/com/pamir/customer/CustomerListDataAccessService.java
+++ src/main/java/com/pamir/customer/CustomerListDataAccessService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class CustomerDataAccessService implements CustomerDao {
+public class CustomerListDataAccessService implements CustomerDao {
 
     //fake database
     private static final List<Customer> customers;
