e414cead79ed51c36e3f15cc7e8a8fdc5ac2b473
Pamir khan <pmysteryk@gmail.com>
2023-05-03 00:17:10 +0500

    feat: added CommandLineRunner method to add some test data in db

--- src/main/java/com/pamir/Main.java
+++ src/main/java/com/pamir/Main.java
@@ -1,7 +1,13 @@
 package com.pamir;
 
+import com.pamir.customer.Customer;
+import com.pamir.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -16,4 +22,14 @@ public class Main {
         }
     }
 
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            var customers = List.of(
+                    new Customer(1, "John", "john@gmail.com", 20),
+                    new Customer(2, "Jamila", "Jamila@gmail.com", 30));
+
+            customerRepository.saveAll(customers);
+        };
+    }
 }
