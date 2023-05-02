2251c9fa53f6573dc23a7d55ab7f62c67827ad37
Pamir khan <pmysteryk@gmail.com>
2023-05-02 07:44:33 +0500

    fix: updated the Customer class

--- src/main/java/com/pamir/customer/Customer.java
+++ src/main/java/com/pamir/customer/Customer.java
@@ -6,19 +6,29 @@ import java.util.Objects;
 
 @Entity
 public class Customer {
+
     @Id
     @SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
-    Integer id;
-    String name;
-    String email;
-    Integer age;
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private Integer age;
 
     public Customer() {
     }
 
     public Customer(Integer id, String name, String email, Integer age) {
         this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer(String name, String email, Integer age) {
         this.name = name;
         this.email = email;
         this.age = age;
@@ -70,13 +80,7 @@ public class Customer {
     }
 
     @Override
-    public String
-    toString() {
-        return "Customer{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", age=" + age +
-                '}';
+    public String toString() {
+        return "Customer{" + "id='" + id + '\'' + ", name='" + name + '\'' + ", email='" + email + '\'' + ", age=" + age + '}';
     }
 }
