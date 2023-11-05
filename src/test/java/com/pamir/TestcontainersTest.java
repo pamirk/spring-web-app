0f56c495f94896cd1077f192409aae6d18cb8dc8
Pamir khan <pmysteryk@gmail.com>
2023-11-05 13:36:33 +0500

    add AbstractTestcontainers as a base class

--- src/test/java/com/pamir/TestcontainersTest.java
+++ src/test/java/com/pamir/TestcontainersTest.java
@@ -1,39 +1,15 @@
 package com.pamir;
 
-import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-public class TestcontainersTest {
-
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("dao-unit-test")
-                    .withUsername("pamir")
-                    .withPassword("password");
+public class TestcontainersTest extends AbstractTestcontainers {
 
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-    }
-
-    @Test
-    void canApplyDBMigrationsWithFlyway() {
-        Flyway flyway = Flyway
-                .configure()
-                .dataSource(
-                        postgreSQLContainer.getJdbcUrl(),
-                        postgreSQLContainer.getUsername(),
-                        postgreSQLContainer.getPassword()
-                ).load();
-        flyway.migrate();
     }
 
 }
