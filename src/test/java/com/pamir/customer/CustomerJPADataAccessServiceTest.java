9ac8fe8c9e86f2bd7a3c403e0a25060d140d3efc
Pamir khan <pmysteryk@gmail.com>
2023-11-08 10:19:57 +0500

    add mock customerRepository dependency using Mockito

--- src/test/java/com/pamir/customer/CustomerJPADataAccessServiceTest.java
+++ src/test/java/com/pamir/customer/CustomerJPADataAccessServiceTest.java
@@ -1,10 +1,30 @@
 package com.pamir.customer;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerJPADataAccessServiceTest {
+
+    private CustomerJPADataAccessService underTest;
+    private AutoCloseable autoCloseable;
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new CustomerJPADataAccessService(customerRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
 
     @Test
     void selectAllCustomers() {
