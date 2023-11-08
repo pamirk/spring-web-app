125293a54abf8e7db94825e6693f19204948e7a6
Pamir khan <pmysteryk@gmail.com>
2023-11-08 10:04:50 +0500

    add CustomerJPADataAccessServiceTest class

---
+++ src/test/java/com/pamir/customer/CustomerJPADataAccessServiceTest.java
@@ -0,0 +1,44 @@
+package com.pamir.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerJPADataAccessServiceTest {
+
+    @Test
+    void selectAllCustomers() {
+    }
+
+    @Test
+    void selectCustomerById() {
+    }
+
+    @Test
+    void insertCustomer() {
+    }
+
+    @Test
+    void existsCustomerWithEmail() {
+    }
+
+    @Test
+    void existsCustomerById() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void selectUserByEmail() {
+    }
+
+    @Test
+    void updateCustomerProfileImageId() {
+    }
+}
