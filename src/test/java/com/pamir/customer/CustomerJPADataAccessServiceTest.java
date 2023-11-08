c2769726fbc6e0cb10320e942ec7dca584aeb71c
Pamir khan <pmysteryk@gmail.com>
2023-11-08 10:38:50 +0500

    add test method updateCustomer

--- src/test/java/com/pamir/customer/CustomerJPADataAccessServiceTest.java
+++ src/test/java/com/pamir/customer/CustomerJPADataAccessServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class CustomerJPADataAccessServiceTest {
@@ -114,7 +113,16 @@ class CustomerJPADataAccessServiceTest {
 
     @Test
     void updateCustomer() {
+        // Given
+        Customer customer = new Customer(1, "Pamir", "Pamir@gmail.com", 2);
+
+        // When
+        underTest.updateCustomer(customer);
+
+        // Then
+        verify(customerRepository).save(customer);
     }
+
 
     @Test
     void selectUserByEmail() {
