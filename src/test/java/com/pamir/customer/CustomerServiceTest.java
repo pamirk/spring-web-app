de17ccd04bccbcb56d80bbcbdbe29174fe47dd39
Pamir khan <pmysteryk@gmail.com>
2023-11-09 09:43:55 +0500

    add dependency for CustomerServiceTest class

--- src/test/java/com/pamir/customer/CustomerServiceTest.java
+++ src/test/java/com/pamir/customer/CustomerServiceTest.java
@@ -3,13 +3,21 @@ package com.pamir.customer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
+
+    @Mock
+    private CustomerDao customerDao;
+    private CustomerService underTest;
+
 
     @BeforeEach
     void setUp() {
+        underTest = new CustomerService(customerDao);
     }
 
     @AfterEach
