3bbabc0d68b30eec13c56407116e16f96ecfbd87
Pamir khan <pmysteryk@gmail.com>
2023-11-09 10:07:51 +0500

    add test method getCustomerById

--- src/test/java/com/pamir/customer/CustomerServiceTest.java
+++ src/test/java/com/pamir/customer/CustomerServiceTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
@@ -37,6 +42,15 @@ class CustomerServiceTest {
 
     @Test
     void getCustomerById() {
+        int id = 10;
+        Customer customer = new Customer(id, "Pamir", "pamir@gmail.com", 19);
+        when(customerDao.selectCustomerById(id)).thenReturn(Optional.of(customer));
+
+        // When
+        Customer actual = underTest.getCustomerById(id);
+
+        // Then
+        assertThat(actual).isEqualTo(customer);
     }
 
     @Test
