6670c261dd40f7796d78db02b069925da1b41887
Pamir khan <pmysteryk@gmail.com>
2023-11-09 09:40:36 +0500

    add CustomerServiceTest class

---
+++ src/test/java/com/pamir/customer/CustomerServiceTest.java
@@ -0,0 +1,38 @@
+package com.pamir.customer;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void getCustomerById() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void deleteCustomerById() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+}
